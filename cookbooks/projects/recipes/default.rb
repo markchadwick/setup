ba7a924283167a34a35cb263a9929c6f44d7fdab
Mark Chadwick <mark.chadwick@gmail.com>
2013-03-16 22:21:18 -0400

    Self-install and update

--- cookbooks/projects/recipes/default.rb
+++ cookbooks/projects/recipes/default.rb
@@ -9,3 +9,20 @@ end
 
 include_recipe 'projects::work'
 include_recipe 'projects::honk'
+
+# -----------------------------------------------------------------------------
+# Self-install
+# -----------------------------------------------------------------------------
+
+projects_project 'setup' do
+  repository  'https://github.com/markchadwick/setup.git'
+  force       true
+end
+
+cron 'create-setup-cron-task' do
+  hour    "4"
+  minute  "0"
+  mailto  "mark.chadwick+setup-#{`hostname`.strip()}@gmail.com"
+  command "cd #{node[:projects][:home]}/setup && make > /dev/null"
+  user    node[:user]
+end
